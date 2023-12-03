6faa1492b6c83037146c02bb0a05dc0442f92406
aelvismorales <faviotio123@gmail.com>
2023-12-03 14:10:36 -0500

    refactor migrations no hay cambios significativos

--- test_auth.py
+++ test_auth.py
@@ -1,8 +1,7 @@
 import pytest
-from flask import json
 from app import create_app
-from app.models.models import db, Usuario, Role
-
+#from app.models.models import db, Usuario, Role
+from werkzeug.security import generate_password_hash,check_password_hash
 
 @pytest.fixture(scope='module')
 def client():
@@ -10,7 +9,10 @@ def client():
     Fixture to create a test client for the Flask application.
     """
     app = create_app('testing')
+    app.testing = True
     with app.test_client() as client:
+        #with app.app_context():
+        #    db.create_all()
         yield client
 
 
@@ -23,20 +25,20 @@ headers = {
 def test_registro(client):
     # Test new user registration
     response = client.post('/auth/registro', json={
-        'nombre': 'testuser',
-        'contraseña': 'testpassword',
+        'nombre': 'testuser3',
+        'contraseña': 'testpassword3',
         'rol': 'Administrador'
     }, headers=headers)
 
     assert response.status_code == 201 or response.status_code == 409
     assert response.get_json()['http_code'] in [201, 409]
     if response.get_json().keys().__contains__('usuario'):
-        assert response.get_json()['usuario'] == 'testuser'
+        assert response.get_json()['usuario'] == 'testuser3'
 
     # Test existing user registration
     response = client.post('/auth/registro', json={
-        'nombre': 'testuser',
-        'contraseña': 'testpassword',
+        'nombre': 'testuser3',
+        'contraseña': 'testpassword3',
         'rol': 'Administrador'
     }, headers=headers)
 
@@ -44,13 +46,34 @@ def test_registro(client):
     assert response.get_json()['http_code'] == 409
 
     # Test non-existing role
+    #Por que se genera otra contraseña si hemos puesto la misma?
     response = client.post('/auth/registro', json={
-        'nombre': 'testuser',
-        'contraseña': 'testpassword',
+        'nombre': 'testuser3',
+        'contraseña': 'testpassword3',
         'rol': 'test'
     }, headers=headers)
 
     assert response.status_code == 409
     assert response.get_json()['http_code'] == 409
 
+
+def test_login(client):
+    # Test not sending data
+    response = client.post('/auth/login', json={},headers=headers)
+    assert response.status_code == 400
+
+    # Test successful login
+    response = client.post('/auth/login', json={
+        'nombre': 'testuser3',
+        'contraseña': 'testpassword3'
+    },headers=headers)
+    assert response.status_code == 200
+
+    # Test unsuccessful login
+    response = client.post('/auth/login', json={
+        'nombre': 'testuser3',
+        'contraseña': 'wrongpassword'
+    },headers=headers)
+    assert response.status_code == 400
+    assert 'Usuario o Contraseña incorrectos' in response.get_json()['mensaje']
 
