9119d81c46e0d62bd64117ddd755251e37e5837a
aelvismorales <faviotio123@gmail.com>
2023-12-03 14:09:33 -0500

    feature Verifica si existe la carpeta migraciones y actualiza a la ultima version de DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,30 @@
 from flask import Flask
 from config import config,CORS_CONFIG
 from flask_cors  import CORS
-from .routes import auth_scope,producto_scope,articulo_scope,nota_scope,mesa_scope
+from app.routes import auth_scope,producto_scope,articulo_scope,nota_scope,mesa_scope
+from app.models.models import db,Role,Tipo,Imagen
+from flask_migrate import Migrate,upgrade,init,migrate
 
-from .models.models import db,Role,Tipo,Imagen
-from flask_migrate import Migrate
+import os
 
-migrate=Migrate()
+migration=Migrate()
 cors=CORS()
 
 def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config.get(config_name,"default"))
     db.init_app(app)
+    migration.init_app(app,db)
     with app.app_context():
         db.create_all()
         Role.insertar_roles()
         Tipo.insertar_tipos()
         Imagen.insertar_fotos()
-    migrate.init_app(app,db)
+        if os.path.exists("migrations"):
+            upgrade()
+        else:
+            init()
+            migrate(message="Initial database migration")
     cors.init_app(app, **CORS_CONFIG)
     
     app.register_blueprint(auth_scope,url_prefix="/auth")
